82018f4a9cbed63899fdd41e8cecd1e8e2c75c17
spandan-hotstar <spandan.buragohain@hotstar.com>
2023-08-16 14:10:23 +0530

    add notes on fixing color for tmux

--- after/plugin/material.lua
+++ after/plugin/material.lua
@@ -1,2 +1,9 @@
+-- If using TMUX ensure the following config is set on `~/.tmux.conf`
+-- set -g default-terminal "xterm-256color"
+-- set -ga terminal-overrides ",*256col*:Tc"
+--
+-- And the following config in `~/.zshrc`
+-- export TERM=xterm-256color
+
 vim.cmd("colorscheme material")
 vim.g.material_style = "darker"
