3ddac36c24b01b7c4aa7d0c16cf8c1f8deafe3ca
spandan-hotstar <spandan.buragohain@hotstar.com>
2024-03-21 08:34:27 +0530

    Add commenter package

--- lua/sudokid/packer.lua
+++ lua/sudokid/packer.lua
@@ -4,11 +4,7 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-
+local function essential(use)
   -- Fuzzy Finder for file finding
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
@@ -16,29 +12,14 @@ return require('packer').startup(function(use)
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
-  -- Color Scheme
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  -- use('marko-cerovac/material.nvim')
-  use('rebelot/kanagawa.nvim')
-
   -- Tree Sitter
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
   use('nvim-treesitter/nvim-treesitter-context');
   use('nvim-treesitter/playground');
 
-  -- Tabs
-  use('gcmt/taboo.vim');
-
-  -- Undo Tree
-  use('mbbill/undotree');
-
   -- Git
   use('airblade/vim-gitgutter');
   use('tpope/vim-fugitive');
-
 
   -- LSP
   use {
@@ -60,9 +41,6 @@ return require('packer').startup(function(use)
   -- LSP Telescope handler
   use('gbrlsnchs/telescope-lsp-handlers.nvim')
 
-  -- Auto pair brackets, parent, quotes
-  use('townk/vim-autoclose')
-
   -- DAPs
   use('mfussenegger/nvim-dap')
   use('nvim-telescope/telescope-dap.nvim')
@@ -78,7 +56,42 @@ return require('packer').startup(function(use)
     opt = true,
     run = "npm install && npx gulp vsDebugServerBundle && mv dist out"
   }
+end
+
+local function theme(use)
+  -- Color Scheme
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  -- use('marko-cerovac/material.nvim')
+  use('rebelot/kanagawa.nvim')
+end
+
+local function utilities(use)
+  -- Tabs
+  use('gcmt/taboo.vim');
+
+  -- Undo Tree
+  use('mbbill/undotree');
+
+  -- Auto pair brackets, parent, quotes
+  use('townk/vim-autoclose')
 
   -- Harpoon
   use('ThePrimeagen/harpoon')
+
+  -- Vim Commantry: Comment and Uncomment lines / selections
+  -- `gcc` to comment & `gcgc` to uncomment
+  -- in select move `gc` toggles between comment and uncomment
+  use('tpope/vim-commentary')
+end
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  essential(use)
+  theme(use)
+  utilities(use)
 end)
