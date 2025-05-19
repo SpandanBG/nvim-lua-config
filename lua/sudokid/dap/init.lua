f5153a1a1d9b4d4f90e0c268e26b07cbd33e3dab
spandan-hotstar <spandan.buragohain@hotstar.com>
2025-05-20 01:03:16 +0530

    add zig dap

--- lua/sudokid/dap/init.lua
+++ lua/sudokid/dap/init.lua
@@ -2,6 +2,7 @@ require("dapui").setup()
 require("nvim-dap-virtual-text").setup()
 
 require("sudokid.dap.rust")
+require("sudokid.dap.zig")
 require("sudokid.dap.go")
 require("sudokid.dap.remap")
 require("sudokid.dap.javascript")
